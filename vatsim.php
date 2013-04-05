8c8f3e7a09696d219e16aca4a4b9f3c6c8d7a018
Rahul Parkar <rahul.a.parkar@gmail.com>
2013-04-05 03:40:11 +0100

    Removing require of file that is not used.

--- vatsim.php
+++ vatsim.php
@@ -11,8 +11,6 @@ class Vatsim
 
 	function clients()
 	{
-		//require_once("VatsimPHPfiles/customization.txt");
-
 		$file=file("http://fsproshop.com/servinfo/vatsim-data.txt");
 		$allowed="no";
 		$data="";
